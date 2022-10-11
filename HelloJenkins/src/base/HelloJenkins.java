58a6aeb9b1589c0fffcb30a642e84ccd5fbb162a
Gary J Howard <GaryJHoward@gmail.com>
2022-10-11 14:28:11 -0400

    test1

--- HelloJenkins/src/base/HelloJenkins.java
+++ HelloJenkins/src/base/HelloJenkins.java
@@ -3,6 +3,6 @@ package base;
 public class HelloJenkins {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello Jenkins you A******");
+		System.out.println("Hello Jenkins you A******!");
 	}
 }
